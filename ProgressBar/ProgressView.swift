0b3770da7cd561160cf952784c00f1893328348c
Maks Fialko <simkawitch@gmail.com>
2019-06-21 17:16:17 +0600

    fix 1.0

--- ProgressBar/ProgressView.swift
+++ ProgressBar/ProgressView.swift
@@ -31,9 +31,9 @@ class VerticalProgressView: UIView {
     var corner: CGFloat = 0.0 {
         didSet {
             if autoCorner {
-                roundedCorner()
+                updateCornerRadius(withValue: self.frame.size.width/2)
             } else {
-             setCustomCorner()
+                updateCornerRadius(withValue: corner)
             }
         }
     }
@@ -48,25 +48,71 @@ class VerticalProgressView: UIView {
     @IBInspectable var autoCorner: Bool = true {
         didSet{
             if autoCorner {
-                roundedCorner()
+                updateCornerRadius(withValue: self.frame.size.width/2)
             } else {
-                setCustomCorner()
+                updateCornerRadius(withValue: corner)
             }
         }
     }
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    func setup() {
+        setupProgressScaleView()
+    }
+    
     // MARK: - Private variables
     private var progressValue: CGFloat = 0
-
+    
     // MARK: - Lifecycle
     override func awakeFromNib() {
         super.awakeFromNib()
-        self.addSubview(progressScaleView)
         
+    }
+    
+    // MARK: - Public methods
+    
+    public func setProgress(_ ratio: CGFloat) {
+        if ratio < 0 {
+            progressValue = 0
+        } else if ratio > 1 {
+            progressValue = self.frame.size.height
+        } else {
+            progressValue = ratio * self.frame.size.height
+        }
+        updateProgress()
+    }
+    
+    public func setProgressColor(withColor color: UIColor) {
+        progressScaleView.backgroundColor = color
+    }
+    
+    // MARK: - Private methods
+    
+    func updateCornerRadius(withValue: CGFloat) {
+        self.layer.cornerRadius = withValue
+        self.progressScaleView.layer.cornerRadius = withValue
+    }
+    
+    public func updateProgress() {
+        self.heightConstraint.constant = self.progressValue
+        self.layoutIfNeeded()
+    }
+    
+    private func setupProgressScaleView() {
+        self.addSubview(progressScaleView)
         progressScaleView.frame = CGRect(x: 0,
-                            y: 0,
-                            width:self.frame.size.width,
-                            height:self.frame.size.height)
+                                         y: 0,
+                                         width:self.frame.size.width,
+                                         height:self.frame.size.height)
         
         progressScaleView.translatesAutoresizingMaskIntoConstraints = false
         let activeBarConstraintBottom = NSLayoutConstraint(item: progressScaleView, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: 0)
@@ -77,57 +123,5 @@ class VerticalProgressView: UIView {
         self.addConstraints([activeBarConstraintBottom, activeBarConstraintLeading,activeBarConstraintTrailling, heightConstraint])
         self.bounds.size.width = progressScaleView.bounds.size.width
     }
-    
-    // MARK: - Public methods
-    
-    public func setProgress(_ ratio: CGFloat, update: Bool? = true, updataWithAnimation: Bool? = true) {
-        if ratio < 0 {
-            print("Warning: progress can't be negative. Progress will be set to 0 ")
-            progressValue = 0
-        } else if ratio > 1 {
-            print("Progress take values between 0 and 1, where 1 is 100%. Progress will be set to 1")
-            progressValue = self.frame.size.height
-        } else {
-        progressValue = ratio * self.frame.size.height
-        }
-        if update == true {
-            guard let animation = updataWithAnimation else {
-                updateProgress(animation: true)
-                return
-            }
-            updateProgress(animation: animation)
-        }
-    }
-    
-    public func setProgressColor(withColor color: UIColor, animated: Bool? = true) {
-        if animated == true {
-            UIView.animate(withDuration: animationDuration) {
-                self.progressScaleView.backgroundColor = color
-            }
-        }
-        progressScaleView.backgroundColor = color
-    }
-    
-    public func updateProgress(animation: Bool) {
-        if animation {
-            UIView.animate(withDuration: animationDuration) {
-                self.heightConstraint.constant = self.progressValue
-                self.layoutIfNeeded()
-            }
-        } else {
-            self.heightConstraint.constant = self.progressValue
-            self.layoutIfNeeded()
-        }
-    }
-    // MARK: - Private methods
-    
-    func roundedCorner() {
-        self.layer.cornerRadius = self.frame.size.width / 2
-        self.progressScaleView.layer.cornerRadius = self.frame.size.width / 2
-    }
-    
-    func setCustomCorner() {
-        self.layer.cornerRadius = corner
-        self.progressScaleView.layer.cornerRadius = corner
-    }
 }
+
